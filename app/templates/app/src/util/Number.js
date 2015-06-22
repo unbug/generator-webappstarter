048e576ab83108bf5552858e0be84466aad2e847
unbug <tidelgl@gmail.com>
2015-06-22 11:15:04 +0800

    count down and increase number

--- app/templates/app/src/util/Number.js
+++ app/templates/app/src/util/Number.js
@@ -5,7 +5,37 @@ define(function (require, exports, module) {
     });
   }
 
+  /**
+   * count down and increase number
+   * @param option = {
+   *  el,dest,rate,duration
+   * }
+   */
+  function countNum(option){
+    option = option || {};
+    if(!option.el){return ;}
+
+    var total = option.dest || 0,
+      rate = option.rate || 50,
+      duration = option.duration || 1500,
+      totalEl = option.el,
+      curNum = parseInt(totalEl.innerHTML)||0,
+      increase = Math.round(Math.abs(curNum-total)/(duration/rate))||1,
+      countDown = curNum>total;
+    function fn(){
+      if( (!countDown && curNum>=total) || (countDown && curNum<=total) ){
+        totalEl.innerHTML = total;
+      }else{
+        totalEl.innerHTML = curNum;
+        curNum += countDown?(-increase):increase;
+        setTimeout(fn,rate);
+      }
+    }
+    fn();
+  }
+
   return {
-    formatMoney: formatMoney
+    formatMoney: formatMoney,
+    countNum: countNum
   }
 });
