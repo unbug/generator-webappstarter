00a5656083d6fe3261aa63b054d5d52a71ae77c5
unbug <tidelgl@gmail.com>
2015-09-29 21:07:57 +0800

    added fallback

--- app/templates/app/src/util/VirtualDOMLite.js
+++ app/templates/app/src/util/VirtualDOMLite.js
@@ -6,23 +6,25 @@ define(function (require, exports, module) {
     $.fn.superHtml = $.fn.html;
     $.fn.html = diffDOM
       ?function (html,diff) {
-        if(diff && this._diffdom){
-          var VDOM = this[0].cloneNode(),
-            VDD = new diffDOM(diff),
-            VDiff;
-          VDOM.innerHTML = html;
-          VDiff = VDD.diff(this[0], VDOM);
-          if(VDD.apply(this[0], VDiff)){
-            this.eq(0).trigger('virtualdomrendered');
-          }
-          return this;
-        }else{
-          this._diffdom = 1;
-          var res = this.superHtml.apply(this,arguments);
-          this.eq(0).trigger('virtualdomrendered');
-          return res;
+      if(diff && this._diffdom){
+        var VDOM = this[0].cloneNode(),
+          VDD = new diffDOM(diff),
+          VDiff;
+        VDOM.innerHTML = html;
+        VDiff = VDD.diff(this[0], VDOM);
+        //fallback
+        if(!VDD.apply(this[0], VDiff)){
+          this.superHtml.apply(this,arguments);
         }
+        this.eq(0).trigger('virtualdomrendered');
+        return this;
+      }else{
+        this._diffdom = 1;
+        var res = this.superHtml.apply(this,arguments);
+        this.eq(0).trigger('virtualdomrendered');
+        return res;
       }
+    }
       :$.fn.superHtml;
 
   })(window.Zepto);
