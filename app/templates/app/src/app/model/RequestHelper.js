491e2fbd9930b06716c797d2a5b4b0a4e824c82b
unbug <unbug@unbug-mbp.local>
2016-04-29 16:29:35 +0800

    remove define

--- app/templates/app/src/app/model/RequestHelper.js
+++ app/templates/app/src/app/model/RequestHelper.js
@@ -1,55 +1,53 @@
-define(function (require, exports, module) {
-  var getJSON = Core.RequestHandler.getJSON,
-    postJSON = Core.RequestHandler.postJSON,
-    JSONP = Core.RequestHandler.JSONP;
+var getJSON = Core.RequestHandler.getJSON,
+  postJSON = Core.RequestHandler.postJSON,
+  JSONP = Core.RequestHandler.JSONP;
 
-  function request(action, data, callback, scope, options) {
-    options = options || {};
-    var __STORE_ID, conf;
-    data = data || {};
-    data._t = new Date().getTime();
-    __STORE_ID = data.__STORE_ID;
-    delete data.__STORE_ID;
-    conf = {
-      action: action,
-      data: data,
-      complete: function (data) {
-        if (data.success) {
-          scope && scope.set && scope.set(data.data, __STORE_ID);
-        }
-        callback && callback(data.success);
+function request(action, data, callback, scope, options) {
+  options = options || {};
+  var __STORE_ID, conf;
+  data = data || {};
+  data._t = new Date().getTime();
+  __STORE_ID = data.__STORE_ID;
+  delete data.__STORE_ID;
+  conf = {
+    action: action,
+    data: data,
+    complete: function (data) {
+      if (data.success) {
+        scope && scope.set && scope.set(data.data, __STORE_ID);
       }
-    };
-    for (var name in options) conf[name] = options[name];
-    conf.action = action;
-    conf.data = data;
-    getJSON(conf);
-  }
-
-  function post(action, data, callback, scope, options) {
-    options = options || {};
-    var conf = {
-      action: action,
-      data: data,
-      contentType: options.contentType || "application/json;charset=utf-8",
-      complete: function (data) {
-        if (data.success) {
-          scope && scope.set && scope.set(data.data);
-        }
-        callback && callback(data.success);
-      }
-    };
-    for (var name in options) conf[name] = options[name];
-    conf.action = action;
-    conf.data = data;
-    postJSON(conf);
-  }
-
-  module.exports = {
-    getJSON: getJSON,
-    postJSON: postJSON,
-    JSONP: JSONP,
-    request: request,
-    post: post
+      callback && callback(data.success);
+    }
   };
-});
+  for (var name in options) conf[name] = options[name];
+  conf.action = action;
+  conf.data = data;
+  getJSON(conf);
+}
+
+function post(action, data, callback, scope, options) {
+  options = options || {};
+  var conf = {
+    action: action,
+    data: data,
+    contentType: options.contentType || "application/json;charset=utf-8",
+    complete: function (data) {
+      if (data.success) {
+        scope && scope.set && scope.set(data.data);
+      }
+      callback && callback(data.success);
+    }
+  };
+  for (var name in options) conf[name] = options[name];
+  conf.action = action;
+  conf.data = data;
+  postJSON(conf);
+}
+
+module.exports = {
+  getJSON: getJSON,
+  postJSON: postJSON,
+  JSONP: JSONP,
+  request: request,
+  post: post
+};
