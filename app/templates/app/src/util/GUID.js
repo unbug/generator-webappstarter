f44fbf22cfc1cadc73875c2f7d37e261eda6028a
unbug <unbug@unbug-mbp.local>
2016-06-28 11:11:52 +0800

    added len to GUID

--- app/templates/app/src/util/GUID.js
+++ app/templates/app/src/util/GUID.js
@@ -1,8 +1,9 @@
-function GUID() {
-  return 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'.replace(/[x]/g, function(c) {
+function GUID(len) {
+  var res = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'.replace(/[x]/g, function(c) {
     var num = Math.random() *16 | 0, v = c === 'x' ? num : (num&0x3|0x8);
     return v.toString(16);
   });
+  return len?res.substr(0, len):res;
 }
 
 module.exports = GUID;
